04c753521259e4536f2bbe1e5c6f9f05217080cb
Alejandro Flores <sjafs.83@gmail.com>
2021-08-19 04:44:02 -0300

    v1.12

--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -6,19 +6,35 @@ const { Dog, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const dog = {
-  name: 'Pug',
+  id: '762ce62c-c8eb-4347-b2cd-675d26956f24',
+  name: 'scoby',
+  height: '15-22',
+  weight: '5-12',
+  life_span: '15 years',
+  image: 'www.fotodeperro.com/1.jpg',
+  temperamentsArr: [1,2]
 };
 
-describe('Videogame routes', () => {
+describe('Dogs routes', () => {
   before(() => conn.authenticate()
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
   beforeEach(() => Dog.sync({ force: true })
     .then(() => Dog.create(dog)));
-  describe('GET /dogs', () => {
+  describe('GET /dogs, debe poder guardar en la BD una nueva raza', () => {
     it('should get 200', () =>
       agent.get('/dogs').expect(200)
+    );
+  });
+  describe('GET /dog/id, deberia retornar status 200 si existe el ID de Raza', () => {
+    it('should get 200', () =>
+      agent.get('/dogs/9').expect(200)
+    );
+  });
+  describe('GET /dog?name=xxx, deberia retornar status 200 si existe el nombre de Raza', () => {
+    it('should get 200', () =>
+      agent.get('/dogs?name=terrier').expect(200)
     );
   });
 });
